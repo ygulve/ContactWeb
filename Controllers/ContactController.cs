aa824097a06134dcabb26018f2750b522b901aa9
unknown <yogesh.gulve@gmail.com>
2020-08-14 11:39:07 +0530

    File checkin in master

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,168 @@
+﻿using ContactWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactWeb.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        const string baseAddress = "https://localhost:44372/api/";
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            IEnumerable<Contact> contacts = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseAddress);
+                // Called Member default GET All records
+                //GetAsync to send a GET request   
+                // PutAsync to send a PUT request  
+                var responseTask = client.GetAsync("contact");
+                responseTask.Wait();
+
+                //To store result of web api response.   
+                var result = responseTask.Result;
+
+                //If success received   
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Contact>>();
+                    readTask.Wait();
+
+                    contacts = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    contacts = Enumerable.Empty<Contact>();
+                    ModelState.AddModelError("Error", result.ReasonPhrase);
+                }
+            }
+            return View(contacts);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseAddress);
+                    // Called Member default GET All records
+                    //GetAsync to send a GET request   
+                    // PutAsync to send a PUT request  
+                    var responseTask = client.PostAsJsonAsync(baseAddress + "contact", contact);
+                    responseTask.Wait();
+
+                    //To store result of web api response.   
+                    var result = responseTask.Result;
+
+                    //If success received   
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+
+                        ModelState.AddModelError("Error", result.ReasonPhrase);
+                    }
+                }
+            }
+            return View(contact);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Contact contact = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseAddress);
+                // Called Member default GET All records
+                //GetAsync to send a GET request   
+                // PutAsync to send a PUT request  
+                var responseTask = client.GetAsync("contact?id=" + id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Contact>();
+                    readTask.Wait();
+
+                    contact = readTask.Result;
+                }
+                else {
+                    ModelState.AddModelError("Error", result.ReasonPhrase);
+                }
+            }
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Contact contact)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseAddress + "contact");
+
+                //HTTP POST
+                var putTask = client.PutAsJsonAsync<Contact>("contact?Id=" + contact.Id, contact);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index");
+                }
+                else {
+                    ModelState.AddModelError("Error", result.ReasonPhrase);
+                }
+            }
+            return View(contact);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseAddress);
+
+                //HTTP POST
+                var deleteTask = client.DeleteAsync("contact?Id=" + id);
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index");
+                }
+                else {
+                    ModelState.AddModelError("Error", result.ReasonPhrase);
+                }
+            }
+            return RedirectToAction("Index");
+        }      
+
+
+
+    }
+
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactWeb.Models
+{
+    public class Contact
+    {       
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [DisplayName("Email")]
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$",  ErrorMessage = "Please Enter Correct Email Address")]
+        public string Email { get; set; }
+        [DisplayName("Phone number")]        
+        [Required]
+        [MaxLength(10, ErrorMessage = "The Phone number must contains 10 characters")]        
+        [RegularExpression("^[0-9]+", ErrorMessage = "Phone must be numeric")]        
+        public string PhoneNumber { get; set; }
+        [DisplayName("Status")]
+        [Required]
+        public bool Status { get; set; }
+    }
+}
